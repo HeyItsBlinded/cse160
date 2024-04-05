7bfb34404d6fcf3fde2ac3b30d74793a46527d0b
Blinded <66330546+HeyItsBlinded@users.noreply.github.com>
2024-04-05 16:56:48 -0700

    unworking draw a point(1)

--- asg0.js
+++ asg0.js
@@ -1,9 +1,34 @@
+// Vertex shader program
+var VSHADER_SOURCE = 
+    'void main() {\n' +
+    '   gl_Position = Vector4(0.0, 0.0, 0.0, 1.0);\n' + 
+    '   gl_PointSize = 10.0;\n' + 
+    '}\n';
+
+// Fragment shader program
+var FSHADER_SOURCE = 
+    'void main() {\n' +
+    '   gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n' + 
+    '}\n';
+
 function main() {
-    var canvas = document.getElementById('example');
-    if (!canvas) {
-        console.log('Failed to retrieve the <canvas> element');
+    var canvas = document.getElementById('webgl');
+
+    var gl = getWebGLContext(canvas);
+    if (!gl) {
+        console.log('Failed to get the rendering context for WebGL');
+        return;
     }
-    var ctx = canvas.getContext('2d');
-    ctx.fillStyle = 'rgba(0, 0, 255, 1.0)';
-    ctx.fillRect(120, 10, 150, 150);
+
+    // Initialize shaders
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.log('Failed to initialize shaders.');
+        return;
+    }
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    
+    // Draw a point
+    gl.drawArrays(gl.POINTS, 0, 1);
 }
