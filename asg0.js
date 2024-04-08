dfe941b3d35dab11f82d77e1e92e6eff4eeaea8b
Blinded <66330546+HeyItsBlinded@users.noreply.github.com>
2024-04-07 18:28:45 -0700

    draw point (1)

--- asg0.js
+++ asg0.js
@@ -1,12 +1,33 @@
+// vertex shader program
+var VSHADER_SOURCE = 
+    'void main() {\n' + 
+    '   gl_Position = vec4(0.0, 0.0, 0.0, 1.0);\n' + // coords
+    '   gl_PointSize = 10.0;\n' + // set point size
+    '}\n'
+
+// fragment shader program
+var FSHADER_SOURCE = 
+    'void main() {\n' + 
+    '   gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n' + // set color
+    '}\n';
+
 function main() {
     var canvas = document.getElementById('webgl');
-
     var gl = getWebGLContext(canvas);
     if (!gl) {
         console.log('failed');
         return;
     }
 
+    //initialize shaders
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.log('failed');
+        return;
+    }
+
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
+
+    // draw point
+    gl.drawArrays(gl.POINTS, 0, 1);
 }
