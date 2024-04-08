d18479c66dc82dd899a5979e8e272892e2f43d48
Blinded <66330546+HeyItsBlinded@users.noreply.github.com>
2024-04-07 23:09:45 -0700

    italy

--- asg0.js
+++ asg0.js
@@ -8,8 +8,10 @@ var VSHADER_SOURCE =
 
 // fragment shader program
 var FSHADER_SOURCE = 
+    'precision mediump float;\n' + 
+    'uniform vec4 u_FragColor;\n' + // uniform var
     'void main() {\n' + 
-    '   gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n' + // set color
+    '   gl_FragColor = u_FragColor;\n' + // set color
     '}\n';
 
 function main() {
@@ -33,30 +35,45 @@ function main() {
         return;
     }
 
+    // get storage loc of u_FragColor variable
+    var u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
+
     // (event handler) called on mouse click
-    canvas.onmousedown = function(ev) { click(ev, gl, canvas, a_Position); };
+    canvas.onmousedown = function(ev) { click(ev, gl, canvas, a_Position, u_FragColor); };
 
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
     var g_points = [];  // array for mouse click
-    function click(ev, gl, canvas, a_Position) {
+    var g_colors = [];  // array to store color of a point
+    function click(ev, gl, canvas, a_Position, u_FragColor) {
         var x = ev.clientX; // x coord
         var y = ev.clientY; // y coord
         var rect = ev.target.getBoundingClientRect();
 
         x = ((x - rect.left)- canvas.height/2) / (canvas.height/2);
         y = (canvas.width/2 - (y - rect.top)) / (canvas.width/2);
-        g_points.push(x); g_points.push(y); // store coords to array 'g_points'
+        // store coords to array 'g_points'
+        g_points.push([x,y]);
+        // store color to array 'g_colors'
+        if (x >= 0.0 && y >= 0.0) { // 1st quadrant
+            g_colors.push([1.0, 0.0, 0.0, 1.0]); // red
+        } else if (x < 0.0 && y < 0.0) { // 3rd quadrant
+            g_colors.push([0.0, 1.0, 0.0, 1.0]); // green
+        } else {
+            g_colors.push([1.0, 1.0, 1.0, 1.0]); // white
+        }
 
         // clear canvas
         gl.clear(gl.COLOR_BUFFER_BIT);
 
         var len = g_points.length;
-        for (var i = 0; i < len; i += 2) {
+        for (var i = 0; i < len; i ++) {
             // pass pos of a point to the var 'a_Position'
-            gl.vertexAttrib3f(a_Position, g_points[i], g_points[i+1], 0.0);
-
+            var xy = g_points[i];
+            var rgba = g_colors[i];
+            gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
+            gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
             gl.drawArrays(gl.POINTS, 0, 1);
         }
     }
