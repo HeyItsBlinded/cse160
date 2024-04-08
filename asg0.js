3d59076c0d64f907d6a133a8f021be6f7f77205b
Blinded <66330546+HeyItsBlinded@users.noreply.github.com>
2024-04-07 22:36:42 -0700

    mouseclick draw point(s)

--- asg0.js
+++ asg0.js
@@ -33,9 +33,31 @@ function main() {
         return;
     }
 
+    // (event handler) called on mouse click
+    canvas.onmousedown = function(ev) { click(ev, gl, canvas, a_Position); };
+
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    // draw point
-    gl.drawArrays(gl.POINTS, 0, 1);
+    var g_points = [];  // array for mouse click
+    function click(ev, gl, canvas, a_Position) {
+        var x = ev.clientX; // x coord
+        var y = ev.clientY; // y coord
+        var rect = ev.target.getBoundingClientRect();
+
+        x = ((x - rect.left)- canvas.height/2) / (canvas.height/2);
+        y = (canvas.width/2 - (y - rect.top)) / (canvas.width/2);
+        g_points.push(x); g_points.push(y); // store coords to array 'g_points'
+
+        // clear canvas
+        gl.clear(gl.COLOR_BUFFER_BIT);
+
+        var len = g_points.length;
+        for (var i = 0; i < len; i += 2) {
+            // pass pos of a point to the var 'a_Position'
+            gl.vertexAttrib3f(a_Position, g_points[i], g_points[i+1], 0.0);
+
+            gl.drawArrays(gl.POINTS, 0, 1);
+        }
+    }
 }
