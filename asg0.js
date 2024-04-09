bd88b293d3692a05c40fc7aa19612762ffa3506f
Blinded <66330546+HeyItsBlinded@users.noreply.github.com>
2024-04-08 20:49:08 -0700

    multipoints

--- asg0.js
+++ asg0.js
@@ -1,80 +1,80 @@
-// vertex shader program
-var VSHADER_SOURCE = 
-    'attribute vec4 a_Position;\n' + 
-    'void main() {\n' + 
-    '   gl_Position = a_Position;\n' + // coords
-    '   gl_PointSize = 10.0;\n' + // set point size
-    '}\n'
+// MultiPoint.js (c) 2012 matsuda
+// Vertex shader program
+var VSHADER_SOURCE =
+  'attribute vec4 a_Position;\n' +
+  'void main() {\n' +
+  '  gl_Position = a_Position;\n' +
+  '  gl_PointSize = 10.0;\n' +
+  '}\n';
 
-// fragment shader program
-var FSHADER_SOURCE = 
-    'precision mediump float;\n' + 
-    'uniform vec4 u_FragColor;\n' + // uniform var
-    'void main() {\n' + 
-    '   gl_FragColor = u_FragColor;\n' + // set color
-    '}\n';
+// Fragment shader program
+var FSHADER_SOURCE =
+  'void main() {\n' +
+  '  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n' +
+  '}\n';
 
 function main() {
-    var canvas = document.getElementById('webgl');
-    var gl = getWebGLContext(canvas);
-    if (!gl) {
-        console.log('failed');
-        return;
-    }
+  // Retrieve <canvas> element
+  var canvas = document.getElementById('webgl');
 
-    //initialize shaders
-    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
-        console.log('failed');
-        return;
-    }
+  // Get the rendering context for WebGL
+  var gl = getWebGLContext(canvas);
+  if (!gl) {
+    console.log('Failed to get the rendering context for WebGL');
+    return;
+  }
 
-    // get storage loc of attribute variable
-    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
-    if (a_Position < 0) {
-        console.log('failed');
-        return;
-    }
+  // Initialize shaders
+  if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+    console.log('Failed to intialize shaders.');
+    return;
+  }
 
-    // get storage loc of u_FragColor variable
-    var u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
+  // Write the positions of vertices to a vertex shader
+  var n = initVertexBuffers(gl);
+  if (n < 0) {
+    console.log('Failed to set the positions of the vertices');
+    return;
+  }
 
-    // (event handler) called on mouse click
-    canvas.onmousedown = function(ev) { click(ev, gl, canvas, a_Position, u_FragColor); };
+  // Specify the color for clearing <canvas>
+  gl.clearColor(0, 0, 0, 1);
 
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT);
+  // Clear <canvas>
+  gl.clear(gl.COLOR_BUFFER_BIT);
 
-    var g_points = [];  // array for mouse click
-    var g_colors = [];  // array to store color of a point
-    function click(ev, gl, canvas, a_Position, u_FragColor) {
-        var x = ev.clientX; // x coord
-        var y = ev.clientY; // y coord
-        var rect = ev.target.getBoundingClientRect();
+  // Draw three points
+  gl.drawArrays(gl.POINTS, 0, n);
+}
 
-        x = ((x - rect.left)- canvas.height/2) / (canvas.height/2);
-        y = (canvas.width/2 - (y - rect.top)) / (canvas.width/2);
-        // store coords to array 'g_points'
-        g_points.push([x,y]);
-        // store color to array 'g_colors'
-        if (x >= 0.0 && y >= 0.0) { // 1st quadrant
-            g_colors.push([1.0, 0.0, 0.0, 1.0]); // red
-        } else if (x < 0.0 && y < 0.0) { // 3rd quadrant
-            g_colors.push([0.0, 1.0, 0.0, 1.0]); // green
-        } else {
-            g_colors.push([1.0, 1.0, 1.0, 1.0]); // white
-        }
+function initVertexBuffers(gl) {
+  var vertices = new Float32Array([
+    0.0, 0.5,   -0.5, -0.5,   0.5, -0.5
+  ]);
+  var n = 3; // The number of vertices
 
-        // clear canvas
-        gl.clear(gl.COLOR_BUFFER_BIT);
+  // Create a buffer object
+  var vertexBuffer = gl.createBuffer();
+  if (!vertexBuffer) {
+    console.log('Failed to create the buffer object');
+    return -1;
+  }
 
-        var len = g_points.length;
-        for (var i = 0; i < len; i ++) {
-            // pass pos of a point to the var 'a_Position'
-            var xy = g_points[i];
-            var rgba = g_colors[i];
-            gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
-            gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
-            gl.drawArrays(gl.POINTS, 0, 1);
-        }
-    }
+  // Bind the buffer object to target
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+  // Write data into the buffer object
+  gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+  var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+  if (a_Position < 0) {
+    console.log('Failed to get the storage location of a_Position');
+    return -1;
+  }
+  // Assign the buffer object to a_Position variable
+  gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+
+  // Enable the assignment to a_Position variable
+  gl.enableVertexAttribArray(a_Position);
+
+  return n;
 }
