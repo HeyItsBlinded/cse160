a613473d100c20e865ea06e39e75cd554f148816
Blinded <66330546+HeyItsBlinded@users.noreply.github.com>
2024-04-09 15:58:29 -0700

    blue rect

--- asg0.js
+++ asg0.js
@@ -1,3 +1,15 @@
+// to avoid potential scoping issues
+let canvas;
+let ctx;
+
 function main() {
-    
+    canvas = document.getElementById('example');
+    if (!canvas) {
+        console.log('failed to retrieve canvas');
+        return;
+    }
+
+    ctx = canvas.getContext('2d');
+    ctx.fillStyle = 'rgba(0, 0, 255, 1.0)';
+    ctx.fillRect(120, 10, 150, 150);
 }
