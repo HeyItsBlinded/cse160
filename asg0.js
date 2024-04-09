6597d84486218bd46e5e5cec87172537bfba8f8b
Blinded <66330546+HeyItsBlinded@users.noreply.github.com>
2024-04-09 16:20:37 -0700

    arguably further than i've gotten in a week and a half

--- asg0.js
+++ asg0.js
@@ -10,6 +10,10 @@ function main() {
     }
 
     ctx = canvas.getContext('2d');
-    ctx.fillStyle = 'rgba(0, 0, 255, 1.0)';
-    ctx.fillRect(120, 10, 150, 150);
+    ctx.fillStyle = 'rgba(0, 0, 0, 1.0)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function handleDrawEvent() {
+    //
 }
