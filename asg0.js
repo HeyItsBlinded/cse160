7e4f713c2e55e720d1e8a9e58c0f4d1d93484fff
Blinded <66330546+HeyItsBlinded@users.noreply.github.com>
2024-04-09 19:00:43 -0700

    v1 COOKING

--- asg0.js
+++ asg0.js
@@ -10,18 +10,30 @@ function main() {
     }
 
     ctx = canvas.getContext('2d');
+
     ctx.fillStyle = 'rgba(0, 0, 0, 1.0)';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
-function handleDrawEvent() {
-    //
-    ctx.strokeStyle = 'red';
-    
+function drawVector(v, color) {
+    ctx.strokeStyle = color;
     let cx = canvas.width/2;
     let cy = canvas.height/2;
     ctx.beginPath();
     ctx.moveTo(cx, cy);
-    ctx.lineTo(cx + 75, cy + 50);
+    ctx.lineTo(cx+v.elements[0]*20, cy-v.elements[1]*20);
     ctx.stroke();
+}
+
+function handleDrawEvent() {
+    // read the xy coords of v1
+    var v1_xcoord = document.getElementById('v1_xcoord').value;
+    var v1_ycoord = document.getElementById('v1_ycoord').value;
+    var v1 = new Vector3([v1_xcoord, v1_ycoord, 0.0]);
+    
+    // clear the canvas
+    ctx.fillStyle = 'rgba(0, 0, 0, 1.0)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    // call drawVector(v1, "red")
+    drawVector(v1, "red");
 }
