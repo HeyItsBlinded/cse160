f1908b8f11136c61fdf0a52a7e55517db3816084
Blinded <66330546+HeyItsBlinded@users.noreply.github.com>
2024-04-12 01:33:08 -0700

    draw point (2)

--- asg1_folder/asg1.js
+++ asg1_folder/asg1.js
@@ -1,8 +1,10 @@
 // Vertex shader program
 var VSHADER_SOURCE = 
+  'attribute vec4 a_Position;\n' + 
+  'attribute float a_PointSize; \n' +
   'void main() {\n' +
-  '  gl_Position = vec4(0.0, 0.0, 0.0, 1.0);\n' + // Set the vertex coordinates of the point
-  '  gl_PointSize = 10.0;\n' +                    // Set the point size
+  '  gl_Position = a_Position;\n' + 
+  '  gl_PointSize = a_PointSize;\n' +
   '}\n';
 
 // Fragment shader program
@@ -27,6 +29,18 @@ function main() {
     console.log('Failed to intialize shaders.');
     return;
   }
+
+  // get storage location of attribute var
+  var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+  if (a_Position < 0) {
+    console.log('failed to get storage location of a_Position');
+  }
+  var a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize');
+
+  // pass vertex position to attribute var
+  gl.vertexAttrib3f(a_Position, 0.0, 0.0, 0.0);
+  // set vertex pos to attribute var
+  gl.vertexAttrib1f(a_PointSize, 5.0);
 
   // Specify the color for clearing <canvas>
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
