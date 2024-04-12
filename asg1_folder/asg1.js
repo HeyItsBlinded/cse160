2f6ea96480e5bac996843d6fddf08b104450dc96
Blinded <66330546+HeyItsBlinded@users.noreply.github.com>
2024-04-12 01:42:38 -0700

    colored points

--- asg1_folder/asg1.js
+++ asg1_folder/asg1.js
@@ -1,16 +1,15 @@
 // Vertex shader program
-var VSHADER_SOURCE = 
-  'attribute vec4 a_Position;\n' + 
-  'attribute float a_PointSize; \n' +
+var VSHADER_SOURCE =
+  'attribute vec4 a_Position;\n' +
   'void main() {\n' +
-  '  gl_Position = a_Position;\n' + 
-  '  gl_PointSize = a_PointSize;\n' +
+  '  gl_Position = a_Position;\n' +
+  '  gl_PointSize = 10.0;\n' +
   '}\n';
 
 // Fragment shader program
 var FSHADER_SOURCE =
   'void main() {\n' +
-  '  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n' + // Set the point color
+  '  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n' +
   '}\n';
 
 function main() {
@@ -30,24 +29,43 @@ function main() {
     return;
   }
 
-  // get storage location of attribute var
+  // // Get the storage location of a_Position
   var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
   if (a_Position < 0) {
-    console.log('failed to get storage location of a_Position');
+    console.log('Failed to get the storage location of a_Position');
+    return;
   }
-  var a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize');
 
-  // pass vertex position to attribute var
-  gl.vertexAttrib3f(a_Position, 0.0, 0.0, 0.0);
-  // set vertex pos to attribute var
-  gl.vertexAttrib1f(a_PointSize, 5.0);
+  // Register function (event handler) to be called on a mouse press
+  canvas.onmousedown = function(ev){ click(ev, gl, canvas, a_Position); };
 
   // Specify the color for clearing <canvas>
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
   // Clear <canvas>
   gl.clear(gl.COLOR_BUFFER_BIT);
+}
 
-  // Draw a point
-  gl.drawArrays(gl.POINTS, 0, 1);
+var g_points = []; // The array for the position of a mouse press
+function click(ev, gl, canvas, a_Position) {
+  var x = ev.clientX; // x coordinate of a mouse pointer
+  var y = ev.clientY; // y coordinate of a mouse pointer
+  var rect = ev.target.getBoundingClientRect() ;
+
+  x = ((x - rect.left) - canvas.width/2)/(canvas.width/2);
+  y = (canvas.height/2 - (y - rect.top))/(canvas.height/2);
+  // Store the coordinates to g_points array
+  g_points.push(x); g_points.push(y);
+
+  // Clear <canvas>
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  var len = g_points.length;
+  for(var i = 0; i < len; i += 2) {
+    // Pass the position of a point to a_Position variable
+    gl.vertexAttrib3f(a_Position, g_points[i], g_points[i+1], 0.0);
+
+    // Draw
+    gl.drawArrays(gl.POINTS, 0, 1);
+  }
 }
