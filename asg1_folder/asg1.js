ce4e9bcee1051e44928c6c6ac3a453c0fb5b0026
Blinded <66330546+HeyItsBlinded@users.noreply.github.com>
2024-04-12 17:32:33 -0700

    everything but the triangle(s)

--- asg1_folder/asg1.js
+++ asg1_folder/asg1.js
@@ -27,7 +27,8 @@ function setupWebGL() {
     canvas = document.getElementById('webgl');
 
     // Get the rendering context for WebGL
-    gl = getWebGLContext(canvas);
+    // gl = getWebGLContext(canvas);
+    gl = canvas.getContext("webgl", { preserveDrawingBuffer: true});
     if (!gl) {
         console.log('Failed to get the rendering context for WebGL');
     return;
@@ -72,6 +73,7 @@ function addActionsUI() {
     // buttons
     document.getElementById('green').onclick = function() { g_selectedColor = [0.0, 1.0, 0.0, 1.0]; };
     document.getElementById('red').onclick   = function() { g_selectedColor = [1.0, 0.0, 0.0, 1.0]; };
+    document.getElementById('clear').onclick   = function() { g_shapesList = []; renderAllShapes(); };
 
     // sliders
     document.getElementById('redSlide').addEventListener('mouseup',   function() { g_selectedColor[0] = this.value / 100; });
@@ -90,6 +92,8 @@ function main() {
 
     // Register function (event handler) to be called on a mouse press
     canvas.onmousedown = click;
+    canvas.onmousemove = function(ev) { if (ev.buttons == 1) { click(ev) } };
+    // canvas.mousemove = click;
 
     // Specify the color for clearing <canvas>
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
@@ -98,33 +102,33 @@ function main() {
     gl.clear(gl.COLOR_BUFFER_BIT);
 }
 
-class Point {
-    constructor(){
-        this.type = 'point';
-        this.position = [0.0, 0.0, 0.0];
-        this.color = [1.0, 1.0, 1.0, 1.0];
-        this.size = 5.0;
-    }
+// class Point {
+//     constructor(){
+//         this.type = 'point';
+//         this.position = [0.0, 0.0, 0.0];
+//         this.color = [1.0, 1.0, 1.0, 1.0];
+//         this.size = 5.0;
+//     }
 
-    render() {
-        var xy = this.position;
-        var rgba = this.color;
-        var size = this.size;
-        // var xy = g_shapesList[i].position;
-        // var rgba = g_shapesList[i].color;
-        // var size = g_shapesList[i].size;
+//     render() {
+//         var xy = this.position;
+//         var rgba = this.color;
+//         var size = this.size;
+//         // var xy = g_shapesList[i].position;
+//         // var rgba = g_shapesList[i].color;
+//         // var size = g_shapesList[i].size;
 
-        // Pass the position of a point to a_Position variable
-        gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
-        // Pass the color of a point to u_FragColor variable
-        gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
-        // pass the size of a point to u_Size variable
-        gl.uniform1f(u_Size, size);
+//         // Pass the position of a point to a_Position variable
+//         gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
+//         // Pass the color of a point to u_FragColor variable
+//         gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
+//         // pass the size of a point to u_Size variable
+//         gl.uniform1f(u_Size, size);
 
-        // Draw
-        gl.drawArrays(gl.POINTS, 0, 1);
-    }
-}
+//         // Draw
+//         gl.drawArrays(gl.POINTS, 0, 1);
+//     }
+// }
 
 var g_shapesList = [];
 // var g_points = [];  // The array for the position of a mouse press
@@ -157,13 +161,28 @@ function convertCoordinatesEventToGL(ev) {
 }
 
 function renderAllShapes() {
+
+    // check time at start of function
+    var startTime = performance.now();
+
     // Clear <canvas>
     gl.clear(gl.COLOR_BUFFER_BIT);
 
     var len = g_shapesList.length;
-    // var len = g_points.length;
-
     for(var i = 0; i < len; i++) {
         g_shapesList[i].render();
     }
+
+    // check time at end of function. show on page
+    var dur = performance.now() - startTime;
+    sendTextToHTML("numdot: " + len + " ms: " + Math.floor(dur) + " fps: " + Math.floor(10000 / dur) / 10, "numdot");
+}
+
+function sendTextToHTML(text, htmlID) {
+    var element = document.getElementById(htmlID);
+    if (!element) {
+        console.log('failed to get ' + htmlID + " from HTML");
+        return;
+    }
+    element.innerHTML = text;
 }
