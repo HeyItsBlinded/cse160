3f6c2ce5672fef99f39fe3ce6002afe1bf01d94d
Blinded <66330546+HeyItsBlinded@users.noreply.github.com>
2024-04-19 00:07:37 -0700

    one textured cube

--- asg5_folder/asg5.js
+++ asg5_folder/asg5.js
@@ -13,9 +13,9 @@ renderer.setSize(w, h);
 document.body.appendChild(renderer.domElement);
 
 // ----- CAMERA -----
-const fov = 115;
-const aspect = w / h;   // maintains aspect ratio
-const near  = 0.1;
+const fov = 75;
+const aspect = w/h;
+const near = 0.1;
 const far = 5;
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 camera.position.z = 2;
@@ -26,36 +26,44 @@ const scene = new THREE.Scene();
 // ----- LIGHT -----
 // const hemiLight = new THREE.HemisphereLight(0x79f14e, 0x80329c);
 // scene.add(hemiLight);
-const color = 0xFFFFFF;
-const intensity = 3;
-const light = new THREE.DirectionalLight(color, intensity);
-light.position.set(-1, 2, 4);
-scene.add(light);
+
+// const color = 0xFFFFFF;
+// const intensity = 3;
+// const light = new THREE.DirectionalLight(color, intensity);
+// light.position.set(-1, 2, 4);
+// scene.add(light);
 
 // ----- SHAPES -----
 const boxWidth = 1;
 const boxHeight = 1;
 const boxDepth = 1;
 const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
-const material = new THREE.MeshPhongMaterial( {color: 0x44aa88} );
-const cube = new THREE.Mesh(geometry, material);
-// scene.add(cube);
 
-function makeInstance(geometry, color, x) {
-    const material = new THREE.MeshPhongMaterial({color});
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
-    cube.position.x = x;
-    return cube;
-}
+const cubes = []
 
-const cubes = [
-    makeInstance(geometry, 0x44aa88, 0),
-    makeInstance(geometry, 0x8844aa, -2),
-    makeInstance(geometry, 0xaa8844, 2),
-]
+// ----- TEXTURES -----
+const loader = new THREE.TextureLoader();
+const texture = loader.load('wall.jpg');
+texture.colorSpace = THREE.SRGBColorSpace;
+const material = new THREE.MeshBasicMaterial( {
+    map: texture
+});
+
+const cube = new THREE.Mesh(geometry, material);    // SHAPES CONT.
+scene.add(cube);
+cubes.push(cube);
 
 // ----- CONTROLS -----
+function resizeRendererToDisplaySize(renderer) {
+    const canvas = renderer.domElement;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if (needResize) {
+        renderer.setSize(width, height, false);
+    }
+    return needResize;
+}
 // const controls = new OrbitControls(camera, renderer.domElement);
 // controls.enableDamping = true;
 // controls.dampingFactor= 0.03;
@@ -69,8 +77,13 @@ const cubes = [
 
 function render(time) {
     time *= 0.001
+    if (resizeRendererToDisplaySize(renderer)) {
+        const canvas = renderer.domElement;
+        camera.aspect = canvas.clientWidth / canvas.clientHeight;
+        camera.updateProjectionMatrix();
+    }
     cubes.forEach((cube, ndx) => {
-        const speed= 1 + ndx * 0.1;
+        const speed = 0.2 + ndx * 0.1;
         const rot = time * speed;
         cube.rotation.x = rot;
         cube.rotation.y = rot;
