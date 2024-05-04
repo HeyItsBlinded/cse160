e0931c703488fbfd72001cddd1c7e05c019f7c71
Blinded <66330546+HeyItsBlinded@users.noreply.github.com>
2024-05-03 18:02:34 -0700

    up until video 3.1
    very bugged. anon plz fix.

--- asg3_folder/asg3.js
+++ asg3_folder/asg3.js
@@ -1,1 +1,186 @@
-console.log('successful js load');
+// Vertex shader program
+var VSHADER_SOURCE = `
+    precision mediump float;
+    attribute vec4 a_Position;
+    attribute vec2 a_UV;
+    varying vec2 v_UV;
+    uniform mat4 u_ModelMatrix;
+    uniform mat4 u_GlobalRotateMatrix;
+    uniform mat4 u_ViewMatrix;
+    uniform mat4 u_ProjectionMatrix
+    void main() {
+        gl_Position = u_ProjectionMatrix * u_ViewMatrix * u_GlobalRotateMatrix * u_ModelMatrix * a_Position;
+        v_UV = a_UV;
+    }`
+
+// Fragment shader program
+var FSHADER_SOURCE = `
+    precision mediump float;
+    varying vec2 v_UV;
+    uniform vec4 u_FragColor;
+    void main() {
+        gl_FragColor = u_FragColor;
+        gl_FragColor = vec4(v_UV, 1.0, 1.0);
+    }`
+
+// global variables
+let canvas;
+let a_Position;
+let u_FragColor;
+let u_ModelMatrix;
+let u_GlobalRotateMatrix;
+
+let gl;
+let a_UV;
+let u_Size;
+let u_ProjectionMatrix;
+let u_ViewMatrix;
+
+// global vars for ui
+let g_selectedColor = [1.0, 1.0, 1.0, 1.0];
+let g_selectedSize = 5;
+let g_selectedSegment = 5;
+let g_globalAngle = 0;
+
+function setupWebGL() {
+    canvas = document.getElementById('webgl');
+    gl = canvas.getContext("webgl", { preserveDrawingBuffer: true});
+    if (!gl) {
+        console.log('Failed to get the rendering context for WebGL');
+    return;
+    }
+    gl.enable(gl.DEPTH_TEST);
+}
+
+function connectVariablesToGLSL() {
+    // Initialize shaders
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.log('Failed to intialize shaders.');
+        return;
+    }
+    // Get the storage location of a_Position
+    a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    if (a_Position < 0) {
+        console.log('Failed to get the storage location of a_Position');
+        return;
+    }
+    // get storage loc of a_UV
+    a_UV = gl.getAttribLocation(gl.program, 'a_UV');
+    if (a_UV < 0) {
+        console.log('failed to get the storage location of a_UV');
+        return;
+    }
+    // Get the storage location of u_FragColor
+    u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
+    if (!u_FragColor) {
+        console.log('Failed to get the storage location of u_FragColor');
+        return;
+    }
+    // get storage location of u_ModelMatrix
+    u_ModelMatrix = gl.getUniformLocation(gl.program, 'u_ModelMatrix');
+    if (!u_ModelMatrix) {
+        console.log('failed to get the storage location of u_ModelMatrix');
+        return;
+    }
+    // get storage location of u_GlobalRotateMatrix
+    u_GlobalRotateMatrix = gl.getUniformLocation(gl.program, 'u_GlobalRotateMatrix');
+    if (!u_GlobalRotateMatrix) {
+        console.log('failed to get storage location of u_GlobalRotateMatrix');
+        return;
+    }
+    // get storage loc of u_ViewMatrix
+    u_ViewMatrix = gl.getUniformLocation(gl.program, 'u_ViewMatrix');
+    if (!u_ViewMatrix) {
+        console.log('failed to get the storage location of u_ViewMatrix');
+        return;
+    }
+    // set initial value for this matrix to identity
+    var identityM = new Matrix4();
+    gl.uniformMatrix4fv(u_ModelMatrix, false, identityM.elements);
+}
+
+// html functionality implementation
+function addActionsUI() {
+
+    // CAMERA ANGLE SLIDER
+    document.getElementById('angleSlide').addEventListener('mousemove', function() { g_globalAngle = this.value; renderAllShapes(); });
+}
+
+// function click(ev) {
+//     // extract event click, return it in webGL coords
+//     let [x, y] = convertCoordinatesEventToGL(ev);
+//     // create and store new point
+//     let point;
+//     if (g_selectedType == POINT) {
+//         point = new Point();
+//     } else if (g_selectedType == TRIANGLE) {
+//         point = new Triangle();
+//     } else  if (g_selectedType == CIRCLE) {
+//         point = new Circle();
+//         point.segments = g_selectedSegment;
+//     }
+//     point.position = [x,y];
+//     point.color = g_selectedColor.slice();
+//     point.size = g_selectedSize;
+//     g_shapesList.push(point);
+//     // draw all shapes in/on the canvas
+//     renderAllShapes();
+// }
+
+function convertCoordinatesEventToGL(ev) {
+    var x = ev.clientX; // x coordinate of a mouse pointer
+    var y = ev.clientY; // y coordinate of a mouse pointer
+    var rect = ev.target.getBoundingClientRect();
+    x = ((x - rect.left) - canvas.width/2)/(canvas.width/2);
+    y = (canvas.height/2 - (y - rect.top))/(canvas.height/2);
+    return ([x, y]);
+}
+
+function sendTextToHTML(text, htmlID) {
+    var element = document.getElementById(htmlID);
+    if (!element) {
+        console.log('failed to get ' + htmlID + " from HTML");
+        return;
+    }
+    element.innerHTML = text;
+}
+
+function main() {
+    setupWebGL();
+    connectVariablesToGLSL();
+    addActionsUI();
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    renderAllShapes();
+    // requestAnimationFrame(tick);
+}
+
+var g_startTime = performance.now() / 1000.0;
+var g_seconds = performance.now() / 1000.0 - g_startTime;
+
+function tick() {
+    // g_seconds = performance.now() / 1000.0 - g_startTime;
+    // // console.log(g_seconds); // FOR DEBUG
+    // updateAnimationAngles();
+    // renderAllShapes();
+    // requestAnimationFrame(tick);
+}
+
+// updates angles of everything if currently animated
+function updateAnimationAngles() {
+    // MORE ANIMS HERE
+}
+
+function renderAllShapes() {
+    // pass matrix to u_ModelMatrix attribute
+    var globalRotMat = new Matrix4().rotate(g_globalAngle, 0, 1, 0);
+    gl.uniformMatrix4fv(u_GlobalRotateMatrix, false, globalRotMat.elements);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    // CUBE RENDER -------------------------
+    var cube = new Cube();
+    cube.color = [1, 0, 1, 1];
+    cube.matrix.scale(0.8, 0.8, 0.8);
+    cube.matrix.translate(-0.2, -0.2, 0.0);
+    cube.render();
+}
