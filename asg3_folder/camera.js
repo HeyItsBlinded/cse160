800866b3f79840e94b2247980a67b6bdaa3682c5
Blinded <66330546+HeyItsBlinded@users.noreply.github.com>
2024-05-08 00:22:56 -0700

    last looks 3/7

--- asg3_folder/camera.js
+++ asg3_folder/camera.js
@@ -1,38 +1,28 @@
 class Camera{   // ADAPTED FROM 3.8
     constructor() {
         // SET CAMERA STARTING LOCATION
-        this.eye = new Vector3([4.5, 0, 10.75]);
-        this.at = new Vector3([4.5, 0, -92.25]);
+        this.eye = new Vector3([14.25, 1, 31]);
+        this.at = new Vector3([14.25, 1, -72]);
         this.up = new Vector3([0, 2, 0]);
+
+    // eye:  14.25 1 31 
+    // at:  14.25 1 -72 
+    // up:  0 2 0
     }
 
     forward() {
         this.eye.elements[2] -= 0.25;
         this.at.elements[2] -= 0.25;
-        // var f = this.at.subtract(this.eye);
-        // f = f.divide(f.length());
-        // this.at = this.at.add(f);
-        // this.eye = this.eye.add(f);
     }
 
     back() {
         this.eye.elements[2] += 0.25;
         this.at.elements[2] += 0.25;
-        // var f = this.eye.subtract(this.at);
-        // f = f.divide(f.length());
-        // this.at = this.at.add(f);
-        // this.eye = this.eye.add(f);
     }
 
     left() {
         this.eye.elements[0] -= 0.25;
         this.at.elements[0] -= 0.25;
-        // var f = this.eye.subtract(this.at);
-        // f = f.divide(f.length());
-        // var s = f.cross(this.up);
-        // s = s.divide(s.length());
-        // this.at = this.at.add(s);
-        // this.eye = this.eye.add(s);
     }
 
     right() {
