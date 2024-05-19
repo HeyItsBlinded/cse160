02ee728340178e5e8f3c3dd0710b069a3844dab4
Blinded <66330546+HeyItsBlinded@users.noreply.github.com>
2024-05-18 18:19:36 -0700

    everyhting but 20 shapes 5B

--- asg5_folder/asg5.js
+++ asg5_folder/asg5.js
@@ -1,5 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/Addons.js';
+import { OBJLoader } from 'three/examples/jsm/Addons.js';
+import { MTLLoader } from 'three/examples/jsm/Addons.js';
 
 // -- SET ----------
 const scene = new THREE.Scene();
@@ -8,21 +10,24 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 // -- LIGHTS ----------
-const lightAMB = new THREE.AmbientLight(0xffffff, 0.5);
+const lightAMB = new THREE.AmbientLight(0xffffff, 0.5); // default
 scene.add(lightAMB);
 
-// const lightDIR = new THREE.DirectionalLight(0xff0000, 0.5); // red lightframe
-// lightDIR.position.set(0, 2, 0);
-// const lightHelper = new THREE.DirectionalLightHelper(lightDIR, 3);
-// scene.add(lightDIR, lightHelper);
+const lightDIR = new THREE.DirectionalLight(0xffffff, 0.5); // overhead
+lightDIR.position.set(0, 12, 0);
+const lightHelper = new THREE.DirectionalLightHelper(lightDIR, 6, 0xff0000); 
+scene.add(lightDIR, lightHelper);
 
-const lightPOINT = new THREE.PointLight(0xffffff, 5, 8, 1); // (color, intensity, distance, decay)
-lightPOINT.position.set(2, 2, 2);
+const lightPOINT = new THREE.PointLight(0xffffff, 5, 8, 1);
+lightPOINT.position.set(0, 8, 0);
 const lightHelper2 = new THREE.PointLightHelper(lightPOINT, 0.5);
 scene.add(lightPOINT, lightHelper2);
 
 // -- CAMERA ----------
 const camera = new THREE.PerspectiveCamera(76, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.x = 0;
+camera.position.y = 10;
+camera.position.z = 15;
 // ORBIT CONTROLS
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.update();
@@ -42,14 +47,44 @@ var skyboxARRAY = [
 var skybox = new THREE.Mesh(skyboxGEO, skyboxARRAY);
 scene.add(skybox);
 
-// -- PROPS ----------
-const cubeGEO = new THREE.BoxGeometry(1, 1, 1);
-const cubeMAT = new THREE.MeshPhongMaterial( {color: 0x9e42f5} );
+// -- GROUND ----------
+const cubeGEO = new THREE.BoxGeometry(20, 0.25, 20);
+const cubeMAT = new THREE.MeshPhongMaterial( {color: 0xa3a3a3} );
 const cube = new THREE.Mesh(cubeGEO, cubeMAT);
 scene.add(cube);
 
-camera.position.z = 5;
+// ----- (BLENDER) OBJS -----
+const objLoader = new OBJLoader();
+objLoader.load('customModels/table.obj', (root) => {
+    root.scale.set(0.75,0.5,0.5);
+    root.rotation.set(THREE.MathUtils.degToRad(-90), THREE.MathUtils.degToRad(0), THREE.MathUtils.degToRad(45));
+    root.position.set(0, 0.1, 0);
 
+    const material = new THREE.MeshPhongMaterial({ color: 0x5e3a0a }); // Custom color material
+    root.traverse((child) => {
+        if (child.isMesh) {
+            child.material = material;
+        }
+    });
+    scene.add(root);
+});
+
+// VR-Mobil by Vladimir Ilic [CC-BY] via Poly Pizza
+// const mtlLoader = new MTLLoader();
+// mtlLoader.load('customModels/vespaMAT.mtl', (mtl) => {
+//     mtl.preload();
+    
+//     const objLoader = new OBJLoader();
+//     objLoader.setMaterials(mtl);
+    
+//     objLoader.load('customModels/vespa.obj', (root) => {
+//         root.scale.set(0.5, 0.5, 0.5);
+//         root.position.set(-1, 0, -0.5);
+//         scene.add(root);
+//     });
+// });
+
+// -- FUNCTIONS ----------
 function animate() {
     requestAnimationFrame(animate);
     controls.update();
